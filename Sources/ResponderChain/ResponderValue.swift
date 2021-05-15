64d7fe4cb380d207598ccc5c3feea3f0dba70369
godL <547188371@qq.com>
2021-05-15 21:40:59 +0800

    make ResponderValue internal

--- Sources/ResponderChain/ResponderValue.swift
+++ Sources/ResponderChain/ResponderValue.swift
@@ -5,16 +5,16 @@
 //  Created by GodL on 2021/4/10.
 //
 
-public struct ResponderValue {
+internal struct ResponderValue {
     let _value: () -> Any
     
-    public init<Value>(_ value: Value) {
+    internal init<Value>(_ value: Value) {
         _value = {
             return value
         }
     }
     
-    public func get<Value>() -> Value {
+    internal func get<Value>() -> Value {
         guard let value = _value() as? Value else { fatalError() }
         return value
     }
